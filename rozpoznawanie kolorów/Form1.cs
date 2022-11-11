74d7d2db9508d4ce54ab3d580d8265304ebcaa80
FilipHojan <81771162+FilipHojan@users.noreply.github.com>
2022-11-11 01:09:44 +0100

    2 zmiana

--- rozpoznawanie kolorów/Form1.cs
+++ rozpoznawanie kolorów/Form1.cs
@@ -22,17 +22,89 @@ namespace rozpoznawanie_kolorów
 
         }
 
+
+
+        public bool process(Bitmap bmp)
+        {
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    var pix = bmp.GetPixel(i, j);
+
+                    if (pix.R > 10 && pix.G > 10 && pix.R > 10)
+                    {
+                        bmp.SetPixel(i, j, Color.White);
+                    }
+                    else
+                    {
+                        bmp.SetPixel(i, j, Color.Black);
+                    }
+
+                }
+            }
+            return true;
+        }
+
+        public bool process2(Bitmap bmp)
+        {
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    var pix = bmp.GetPixel(i, j);
+
+                    if (pix.R > 120 && pix.G > 120 && pix.R > 120)
+                    {
+                        bmp.SetPixel(i, j, Color.White);
+                    }
+                    else
+                    {
+                        bmp.SetPixel(i, j, Color.Green);
+                    }
+
+                }
+            }
+            return true;
+        }
+
+
+
+
+
+
+
+
+
         private void button2_Click(object sender, EventArgs e)
         {
-
+            Bitmap copyImage = new Bitmap((Bitmap)pictureBox1.Image);
+            process(copyImage);
+            pictureBox1.Image = copyImage;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Select your image";
+            openFileDialog1.Filter = "Image file *.jpg; *.jpeg; *.bmp; | *.jpg; *.jpeg; *.bmp;";
 
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap image = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = image;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap copyImage = new Bitmap((Bitmap)pictureBox1.Image);
+            process2(copyImage);
+            pictureBox1.Image = copyImage;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
